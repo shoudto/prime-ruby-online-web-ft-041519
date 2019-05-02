570425e5218d017d6c7be540e9723d4a4b9af185
Oscar Shoudt <shoudto@gmail.com>
2019-05-02 16:33:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,8 +6,10 @@ require 'prime'
 def prime?(number)
   # binding.pry 
   return false if number < 2 
-  (2..number - 1 ).each {|num| if (number % num) == 0}
+  (2..number - 1 ).each do|num| 
+    if (number % num) == 0
     return false
+    end
   end 
     true
 end 
